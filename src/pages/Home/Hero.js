9aae32178108a4eec21bdaf3daecc5a9ed37c6b2
wojtko <j.w2001@onet.pl>
2023-12-20 15:20:33 +0100

    Improve hero section. Add cards

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -1,3 +1,4 @@
+import { TaskOutlined } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
@@ -6,7 +7,7 @@ function Hero() {
     <Container>
       <TextHero>
         <Heading>
-          Easily<br></br> organize <br></br>your work.
+          Easily<br></br> organize <br></br>your work
         </Heading>
         <SubHeading>
           Simplify your life with our sleek todo list website for effortless
@@ -14,7 +15,29 @@ function Hero() {
         </SubHeading>
         <Button>Discover</Button>
       </TextHero>
-      <Aside></Aside>
+
+      <Cards>
+        <TaskCard>
+          <CircleIcon>
+            <TaskOutlined />
+          </CircleIcon>
+          <CardHeading>Manage your Task</CardHeading>
+          <CardSubHeading>
+            Stay organized and on top of Your Tasks: The ultimmate Task
+            Management Solution
+          </CardSubHeading>
+        </TaskCard>
+        <ProjectsCard>
+          <CircleIcon>
+            <TaskOutlined />
+          </CircleIcon>
+          <CardHeading>Manage your Projects</CardHeading>
+          <CardSubHeading>
+            Help your team streamline your work and boost productivity: Powerful
+            Project Management Made Simple
+          </CardSubHeading>
+        </ProjectsCard>
+      </Cards>
     </Container>
   );
 }
@@ -24,8 +47,10 @@ export default Hero;
 const Container = styled.div`
   flex: 1;
   display: grid;
+  place-items: center;
   grid-template-columns: 1fr 1fr;
   padding: 64px 0;
+  min-height: calc(100vh - 82px);
 `;
 
 const TextHero = styled.div`
@@ -36,11 +61,13 @@ const TextHero = styled.div`
 
 const Heading = styled.h1`
   font-size: clamp(3rem, 7.5vw, 5rem);
+  letter-spacing: -0.5px;
 `;
 
 const SubHeading = styled.p`
   font-size: 24px;
   line-height: 1.5;
+  width: 75%;
   margin-bottom: 36px;
 `;
 
@@ -61,4 +88,58 @@ const Button = styled.button`
   }
 `;
 
-const Aside = styled.div``;
+const Cards = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  place-items: end;
+`;
+
+const TaskCard = styled.div`
+  position: relative;
+  width: 300px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  border: 3px solid #313131;
+  border-radius: 12px;
+  grid-row: 2;
+  padding: 10px;
+`;
+
+const ProjectsCard = styled(TaskCard)`
+  width: 260px;
+  height: 260px;
+  border-color: #7743db;
+  grid-row: 1;
+  grid-column: 3;
+`;
+
+const CardHeading = styled.h2`
+  text-align: center;
+  letter-spacing: -0.5px;
+`;
+
+const CardSubHeading = styled.p`
+  text-align: center;
+  line-height: 1.5;
+  opacity: 0.75;
+`;
+
+const CircleIcon = styled.div`
+  position: absolute;
+  background: #c3acd0;
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  display: grid;
+  place-items: center;
+  top: -36px;
+
+  .MuiSvgIcon-root {
+    color: #f9f9f9 !important;
+  }
+`;
