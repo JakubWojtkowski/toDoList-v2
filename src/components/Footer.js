1d6dda251a75fbf16351449bf4d43aeb14bd7112
wojtko <j.w2001@onet.pl>
2024-01-10 23:20:09 +0100

    Minor changes

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer() {
+  return <Container>footer</Container>;
+}
+
+export default Footer;
+
+const Container = styled.div``;
